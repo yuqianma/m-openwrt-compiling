d7ece0359c6febda0b0bd68447af378f334b22b6
Yuqian Ma <mayq0422@gmail.com>
2024-12-20 06:30:02 +0000

    update helloworld branch; update jd & d1

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@
 # Uncomment a feed source
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
